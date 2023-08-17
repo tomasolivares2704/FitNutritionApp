128adad88e3da7926d0752ac0e2cbf1a6afeba79
tomasolivares2704 <129810321+tomasolivares2704@users.noreply.github.com>
2023-08-16 21:19:18 -0400

    CREACION TAB EN HOME
    TOMAS

--- FitNutritionApp/src/app/app.component.ts
+++ FitNutritionApp/src/app/app.component.ts
@@ -6,12 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Home', url: '/home', icon: 'paper-plane' },
     { title: 'Food', url: 'food', icon: 'heart' },
     { title: 'Training', url: '/training', icon: 'archive' },
     
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = [];
   constructor() {}
 }
